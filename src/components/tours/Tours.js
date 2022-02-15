da7bc8d9376a1edb5bae077b335309c21d62331e
e97m <majdalawi97@yahoo.com>
2022-02-15 10:49:20 +0200

    not solved, send id

--- src/components/tours/Tours.js
+++ src/components/tours/Tours.js
@@ -1,15 +1,17 @@
 import './Tours.css';
 import Tour from './tour/Tour'
 import citiesData from '../../data/db.json'
-
+import { Link } from "react-router-dom";
 
 function Tours () {
   return (
     <>
        <div className="Tours">
         {
-          citiesData.map(element => (      
-            <Tour card={element} idKey={element.id}/>
+          citiesData.map(element => ( 
+            <div className="card" key ={element.id}>
+            <Link to={`/tour/${element.id}`}>  <Tour card={element} idKey={element.id} />  </Link>
+            </div>
           ))
         }
       </div>
