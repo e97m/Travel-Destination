d4a09367d78fc9c342cac72d382048f95bf09f05
e97m <majdalawi97@yahoo.com>
2022-02-15 18:54:32 +0200

    finally it works

--- src/components/tours/tour/Tour.js
+++ src/components/tours/tour/Tour.js
@@ -1,12 +1,11 @@
 import './Tour.css';
-import TourDetails from '../../tourDetails/TourDetails'
-
 
 function Tour({card , idKey}) {
     // const {card} = props
 
     return (
         <div className='tour' >
+            <br/>
             <h1>Name : {card.name} </h1>
             <img alt='city' src={card.image} className='img' />
             <br />
